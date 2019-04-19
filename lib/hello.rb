acfddbf1aa74e4a0c0be921016160858ba1b7351
Katherine Wren <kwren@outlook.com>
2019-04-19 18:00:49 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,18 +1,9 @@
 def hello_t(array)
 i = 0 
 while i < array.length
-  yield array[i]
+  yield (array[i])
   i = i + 1
 end
 end
 
-# call your method here!
-array = ["Tim", "Tom", "Jim"]
-
-def hello_t(array) 
-do |name|
-  if name.start_with?("T")
-    puts "Hi, #{name}"
-  end
-  
-end
+# call y
